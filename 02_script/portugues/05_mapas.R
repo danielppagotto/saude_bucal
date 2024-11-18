f6723a5ce2ea01a6e5e0aa30d3de7a5d8855700b
Daniel Pagotto <danielppagotto@gmail.com>
2024-11-17 23:59:24 -0300

    update

--- 02_script/portugues/05_mapas.R
+++ 02_script/portugues/05_mapas.R
@@ -55,7 +55,8 @@ baseline <-
   mutate(rr = if_else(rr > 100, 100, rr)) |> 
   left_join(spdf_fortified, 
             by = c("cod_regsaud"="reg_id")) |> 
-  distinct_all()
+  distinct_all() |> 
+  mutate(scenario = "Baseline")
 
 if (!"geometry" %in% names(baseline)) {
   
@@ -183,16 +184,251 @@ b1
 c1 <- a1 + b1 
 c1
 
-ggsave(filename = "04_mapas/cenario_baselinet.png",
+ggsave(filename = "04_mapas/cenario_baseline.png",
        c1, dpi = 350, height = 8, width = 16)
 
 
 
 # APS ---------------------------------------------------------------------
 
-
 # cenario 1 ---------------------------------------------------------------
 
+c2_productivity <- 
+  resultado_teste2 |> 
+  filter(tempo_endo == '35' &
+           tempo_prot == '35' & 
+           tempo_aps == '25' &
+           tempo_peri == '35' &
+           pd == '0.6',
+         pl == '0.6',
+         ttd == '1676',
+         sus == 'Somente profissionais SUS',
+         plano == 'Necessidade de todos')
+
+
+c2_productivity <- 
+  c2_productivity |> 
+  mutate(rr = 100 * rr) |> 
+  mutate(rr = if_else(rr > 100, 100, rr)) |> 
+  left_join(spdf_fortified, 
+            by = c("cod_regsaud"="reg_id")) |> 
+  distinct_all() |> 
+  mutate(scenario = "Scenario 2")
+
+
+c2 <- 
+  ggplot() +
+  geom_sf(data = c2_productivity |> 
+            filter(nivel == "APS"), 
+          aes(fill = rr, geometry = geometry), 
+          color = "#f5f5f5") +
+  geom_sf(data = estados_br, 
+          fill = NA, 
+          color = "#4c4d4a", 
+          size = 0.1) + 
+  geom_sf(data = estados_br |> filter(abbrev_state == "MG" &
+                                        abbrev_state == "SP"), 
+          fill = NA, 
+          color = "black", # Cor preta mais escura
+          size = 2) +
+  scale_fill_gradientn(colors = c("#D92B3A", 
+                                  "#d4e302",
+                                  "#02592e"), 
+                       values = 
+                         rescale(c(0,50,100)), 
+                       limits = c(0,100),
+                       breaks = c(0, 50, 100)) + 
+  theme_minimal() +
+  labs(fill = "Relative gap") +
+  annotation_scale(location = "bl", width_hint = 0.3) +
+  annotate("text", x = -Inf, y = Inf, label = "A", # Canto superior esquerdo
+           hjust = -0.5, vjust = 1.5, size = 6) + 
+  theme(
+    legend.position = "bottom",
+    legend.justification = "center",
+    legend.box = "horizontal",
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    axis.text.x = element_text(size = 14),  
+    axis.text.y = element_text(size = 14),
+    legend.text = element_text(size = 14),
+    plot.title = element_text(size = 16),
+    panel.border = element_rect(color = "black", 
+                                fill = NA, 
+                                size = 1), 
+    plot.margin = margin(10, 10, 10, 10)) +
+  ggtitle("Primary healthcare")
+
+
+c3_productivity <- 
+  resultado_teste2 |> 
+  filter(tempo_endo == '35' &
+           tempo_prot == '35' & 
+           tempo_aps == '25' &
+           tempo_peri == '35' &
+           pd == '0.6',
+         pl == '0.6',
+         ttd == '1676',
+         sus == 'Somente profissionais SUS',
+         plano == "Apenas SUS dependente")
+
+
+c3_productivity <- 
+  c3_productivity |> 
+  mutate(rr = 100 * rr) |> 
+  mutate(rr = if_else(rr > 100, 100, rr)) |> 
+  left_join(spdf_fortified, 
+            by = c("cod_regsaud"="reg_id")) |> 
+  distinct_all() |> 
+  mutate(scenario = "Scenario 3")
+
+c3 <- 
+  ggplot() +
+  geom_sf(data = c3_productivity |> 
+            filter(nivel == "APS"), 
+          aes(fill = rr, geometry = geometry), 
+          color = "#f5f5f5") +
+  geom_sf(data = estados_br, 
+          fill = NA, 
+          color = "#4c4d4a", 
+          size = 0.1) + 
+  geom_sf(data = estados_br |> filter(abbrev_state == "MG" &
+                                        abbrev_state == "SP"), 
+          fill = NA, 
+          color = "black", # Cor preta mais escura
+          size = 2) +
+  scale_fill_gradientn(colors = c("#D92B3A", 
+                                  "#d4e302",
+                                  "#02592e"), 
+                       values = 
+                         rescale(c(0,50,100)), 
+                       limits = c(0,100),
+                       breaks = c(0, 50, 100)) + 
+  theme_minimal() +
+  labs(fill = "Relative gap") +
+  annotation_scale(location = "bl", width_hint = 0.3) +
+  annotate("text", x = -Inf, y = Inf, label = "B", # Canto superior esquerdo
+           hjust = -0.5, vjust = 1.5, size = 6) + 
+  theme(
+    legend.position = "bottom",
+    legend.justification = "center",
+    legend.box = "horizontal",
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    axis.text.x = element_text(size = 14),  
+    axis.text.y = element_text(size = 14),
+    legend.text = element_text(size = 14),
+    plot.title = element_text(size = 16),
+    panel.border = element_rect(color = "black", 
+                                fill = NA, 
+                                size = 1), 
+    plot.margin = margin(10, 10, 10, 10)) 
+
+
+c4_productivity <- 
+  resultado_teste2 |> 
+  filter(tempo_endo == '35' &
+           tempo_prot == '35' & 
+           tempo_aps == '25' &
+           tempo_peri == '35' &
+           pd == '0.6',
+         pl == '0.6',
+         ttd == '1676',
+         sus == "Todos profissionais independente de vínculo",
+         plano == "Apenas SUS dependente")
+
+
+c4_productivity <- 
+  c4_productivity |> 
+  mutate(rr = 100 * rr) |> 
+  mutate(rr = if_else(rr > 100, 100, rr)) |> 
+  left_join(spdf_fortified, 
+            by = c("cod_regsaud"="reg_id")) |> 
+  distinct_all() |> 
+  mutate(scenario = "Scenario 4")
+
+c4 <- 
+  ggplot() +
+  geom_sf(data = c4_productivity |> 
+            filter(nivel == "APS"), 
+          aes(fill = rr, geometry = geometry), 
+          color = "#f5f5f5") +
+  geom_sf(data = estados_br, 
+          fill = NA, 
+          color = "#4c4d4a", 
+          size = 0.1) + 
+   scale_fill_gradientn(colors = c("#D92B3A", 
+                                  "#d4e302",
+                                  "#02592e"), 
+                       values = 
+                         rescale(c(0,50,100)), 
+                       limits = c(0,100),
+                       breaks = c(0, 50, 100)) + 
+  theme_minimal() +
+  labs(fill = "Relative gap") +
+  annotation_scale(location = "bl", width_hint = 0.3) +
+  annotate("text", x = -Inf, y = Inf, label = "C", # Canto superior esquerdo
+           hjust = -0.5, vjust = 1.5, size = 6) + 
+  theme(
+    legend.position = "bottom",
+    legend.justification = "center",
+    legend.box = "horizontal",
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    axis.text.x = element_text(size = 14),  
+    axis.text.y = element_text(size = 14),
+    legend.text = element_text(size = 14),
+    plot.title = element_text(size = 16),
+    panel.border = element_rect(color = "black", 
+                                fill = NA, 
+                                size = 1), 
+    plot.margin = margin(10, 10, 10, 10)) 
+
+
+
+# grafico 
+
+grafico <- rbind(baseline,
+                 c2_productivity,
+                 c3_productivity,
+                 c4_productivity)
+
+evolucao <- grafico |> 
+  filter(nivel == "APS") |> 
+  mutate(regiao = case_when(
+    uf_sigla %in% c("AC", "AP", "AM", "PA", "RO", "RR", "TO") ~ "Norte",
+    uf_sigla %in% c("AL", "BA", "CE", "MA", "PB", "PE", "PI", "RN", "SE") ~ "Nordeste",
+    uf_sigla %in% c("DF", "GO", "MT", "MS") ~ "Centro-Oeste",
+    uf_sigla %in% c("ES", "MG", "RJ", "SP") ~ "Sudeste",
+    uf_sigla %in% c("PR", "RS", "SC") ~ "Sul")) |> 
+  group_by(nivel, scenario, regiao) |> 
+  summarise(necessidade = sum(necessidade),
+            necessidade_sus = sum(necessidade_sus),
+            oferta_rs = sum(oferta_rs)) |>
+  mutate(rr = if_else(scenario == "Baseline" |
+                      scenario == "Scenario 2", 
+         (oferta_rs/necessidade) * 100,
+         (oferta_rs/necessidade_sus) * 100)) |> 
+  mutate(rr = round(rr, 2)) |> 
+  ggplot(aes(x = scenario, y = rr, 
+             col = regiao, group = regiao)) + 
+  geom_line(linetype = "dashed") +
+  geom_label(aes(label = rr), 
+             size = 3, fontface = "bold", label.size = 0.2) +
+  theme_minimal()
+
+evolucao
+
+c_aps <- (a1 + c2 + c4) / evolucao
+
+ggsave(filename = "04_mapas/cenarios_aps.png",
+       c_aps, dpi = 600, height = 8, width = 14)
+
+
+
+
+
+# a partir daqui é backup -------------------------------------------------
 
 
 
